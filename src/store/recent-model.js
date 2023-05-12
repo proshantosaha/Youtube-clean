58314f71ea59de67b69f0db4e98126f2f1dadb28
proshantosaha <proshantoll71@gmail.com>
2023-05-12 16:37:42 +0600

    create favourite model and completed favourite model state management

---
+++ src/store/recent-model.js
@@ -0,0 +1,17 @@
+import {action, persist} from 'easy-peasy'
+
+
+const favouriteModel = persist({
+    items :[],
+    addToFavourite : action((state,playlistId)=>{
+        state.items.push(playlistId)
+    }),
+    removeFavourite : action((state,playlistId)=>{
+       state.items = state.items.filter((pId)=> playlistId !== pId);
+    }),
+})
+
+
+export default favouriteModel
+
+
